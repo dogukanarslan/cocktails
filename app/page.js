4c06e3a798d84dadfdc5a0d578db119a512a73c6
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-05-20 23:34:40 +0300

    Consume original API

--- app/page.js
+++ app/page.js
@@ -1,51 +1,14 @@
-import Link from 'next/link';
-import Image from 'next/image';
+'use client';
 
-import SearchBar from 'components/SearchBar';
+import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
 
-import styles from 'styles/Cocktails.module.css';
+const HomePage = () => {
+  useEffect(() => {
+    redirect('/category/Beer');
+  }, []);
 
-const getData = async () => {
-  const res = await fetch('https://the-cocktail-db3.p.rapidapi.com/', {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-host': process.env.API_HOST,
-      'x-rapidapi-key': process.env.API_KEY
-    }
-  });
-
-  return res.json();
+  return;
 };
 
-const CocktailsPage = async () => {
-  const drinks = await getData();
-
-  return (
-    <>
-      <SearchBar />
-      <div className={styles.drinks}>
-        {drinks?.map((drink) => {
-          return (
-            <div className={styles.card} key={drink.id}>
-              <Image
-                src={drink.image}
-                alt="Drink image"
-                width={200}
-                height={200}
-              />
-              <div>
-                <p>{drink.title}</p>
-                <div>
-                  <span className={styles.badge}>{drink.difficulty}</span>
-                </div>
-                <Link href={drink.id}>View</Link>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
-};
-
-export default CocktailsPage;
+export default HomePage;
--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,33 +1,111 @@
 'use client';
 
+import { useParams, useRouter } from 'next/navigation';
 import { useState } from 'react';
 
-const SearchBar = () => {
-  const [searchValue, setSearchValue] = useState('');
-  const [sortType, setSortType] = useState('name');
+const SearchBar = (props) => {
+  const { categories, glasses, alcoholicOptions, ingredients } = props;
+
+  const router = useRouter();
+  const params = useParams();
+
+  console.log(params);
+
+  const [searchBy, setSearchBy] = useState(params.filter);
+  const [option, setOption] = useState(decodeURI(params.option));
+
+  const changeFilter = (filter) => {
+    setSearchBy(filter);
+    setOption('');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    router.push(`/${searchBy}/${option}`);
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="search">Drink Name</label>
-        <input
-          id="search"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="sort">Sort by</label>
+        <label htmlFor="searchBy">Search by</label>
         <select
-          id="sort"
-          onChange={(e) => setSortType(e.target.value)}
-          value={sortType}
+          id="searchBy"
+          value={searchBy}
+          required
+          onChange={(e) => changeFilter(e.target.value)}
         >
-          <option value="name">Name</option>
-          <option value="difficulty">Difficulty</option>
+          <option value="category">Category</option>
+          <option value="glass">Glass</option>
+          <option value="ingredient">Ingredient</option>
+          <option value="alcoholic">Alcoholic</option>
         </select>
       </div>
+
+      {searchBy === 'category' && (
+        <div>
+          <select
+            value={option}
+            required
+            onChange={(e) => setOption(e.target.value)}
+          >
+            <option value="">Select a category</option>
+            {categories.map((category) => (
+              <option key={category.strCategory} value={category.strCategory}>
+                {category.strCategory}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+
+      {searchBy === 'glass' && (
+        <div>
+          <select
+            value={option}
+            required
+            onChange={(e) => setOption(e.target.value)}
+          >
+            <option value="">Select a glass</option>
+            {glasses.map((glass) => (
+              <option key={glass.strGlass} value={glass.strGlass}>
+                {glass.strGlass}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+
+      {searchBy === 'alcoholic' && (
+        <div>
+          <select
+            value={option}
+            required
+            onChange={(e) => setOption(e.target.value)}
+          >
+            <option value="">Select alcoholic option</option>
+            {alcoholicOptions.map((option) => (
+              <option key={option.strAlcoholic} value={option.strAlcoholic}>
+                {option.strAlcoholic}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+
+      {searchBy === 'ingredient' && (
+        <div>
+          <select value={option} onChange={(e) => setOption(e.target.value)}>
+            <option value="">Select an ingredient</option>
+            {ingredients.map((option) => (
+              <option key={option.strIngredient1} value={option.strIngredient1}>
+                {option.strIngredient1}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+      <button>Fetch</button>
     </form>
   );
 };
