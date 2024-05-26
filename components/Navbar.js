b3467bbb8f5092011f0951eea71bf29f3e427a01
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-05-26 18:47:12 +0300

    Search by drink name

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,11 @@ import styles from 'styles/Navbar.module.css';
 const Navbar = () => (
   <nav className={styles.navbar}>
     <Link href="/">COCKTAILS</Link>
+    <ul className={styles.nav}>
+      <li>
+        <Link href="/search">Search</Link>
+      </li>
+    </ul>
   </nav>
 );
 
