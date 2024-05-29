3f30f8c7a588ecaccc71c0eb5ad5caaa8ada3b73
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-05-30 00:37:28 +0300

    Show icon on tab

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body>
         <Navbar />
         <main>
